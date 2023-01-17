008d47863ee552e00af2e4874c24df18bd3233ed
Akash Perera <akash.perera@chope.co>
2023-01-18 04:35:29 +0530

    capture current location for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "antd/dist/reset.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import FavoritesPage from "./routes/favorites";
 import IndexPage from "./routes/index";
 import Layout from "./routes/layout";
 
 export default function App() {
+  // capture current location for navigation
+  const location = useLocation();
+
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout pathname={location.pathname} />}>
         <Route index element={<IndexPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Route>
