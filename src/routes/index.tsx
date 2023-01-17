2be57e71bbb18192ecc7928a81ade36effd56c45
Akash Perera <akash.perera@chope.co>
2023-01-18 05:07:35 +0530

    add CocktailCard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,16 @@
-import { HeartOutlined, HeartTwoTone } from "@ant-design/icons";
-import { Button, Card } from "antd";
-import Meta from "antd/es/card/Meta";
 import Search from "antd/es/input/Search";
 import { useContext, useState } from "react";
 import styled from "styled-components";
-import CategoryTag from "../components/CategoryTag";
+import CocktailCard from "../components/CocktailCard";
 import Loader from "../components/Loader";
 import { AppContext } from "../contexts/appContext";
-import { Cocktail } from "../types/api";
 
 const IndexPage = (): JSX.Element => {
   // get data from ctx
-  const { cocktailsData, favoriteIds, addFavorite, removeFavorite } =
-    useContext(AppContext);
+  const { cocktailsData } = useContext(AppContext);
 
   // search cocktails using strDrink
   const [searchText, setSearchText] = useState<string>("");
-
-  const onAddFavorites = async (cocktail: Cocktail) => {
-    addFavorite(cocktail);
-  };
-
-  const onRemoveFavorites = async (idDrink: string) => {
-    removeFavorite(idDrink);
-  };
 
   const onInputSearch = (text: string) => {
     // inputs have been converted to lowercase to use with includes()
@@ -59,48 +46,7 @@ const IndexPage = (): JSX.Element => {
             cocktail.strDrink.toLocaleLowerCase().includes(searchText)
           )
           .map((cocktail) => (
-            <Card
-              hoverable
-              className="card"
-              key={cocktail.idDrink}
-              title={cocktail.strDrink}
-              cover={
-                <img
-                  className="card-cover"
-                  alt="card cover"
-                  src={cocktail.strDrinkThumb}
-                />
-              }
-              extra={
-                <>
-                  {favoriteIds.includes(cocktail.idDrink) ? (
-                    <Button
-                      type="ghost"
-                      shape="circle"
-                      icon={<HeartTwoTone twoToneColor="#eb2f96" />}
-                      onClick={() => onRemoveFavorites(cocktail.idDrink)}
-                    />
-                  ) : (
-                    <Button
-                      type="ghost"
-                      shape="circle"
-                      icon={<HeartOutlined />}
-                      onClick={() => onAddFavorites(cocktail)}
-                    />
-                  )}
-                </>
-              }
-            >
-              <Meta
-                description={
-                  <>
-                    <p className="instructions">{cocktail.strInstructions}</p>
-
-                    <CategoryTag name={cocktail.strCategory} />
-                  </>
-                }
-              />
-            </Card>
+            <CocktailCard cocktail={cocktail} />
           ))}
       </div>
     </Container>
@@ -123,57 +69,11 @@ const Container = styled.div`
     grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 
-  .card:not(:last-child) {
-    margin-bottom: 12px;
-  }
-
-  .ant-card-head {
-    padding: 12px;
-  }
-
-  .ant-card-cover {
-    display: flex;
-    flex: 1;
-    justify-content: center;
-  }
-
-  .card-cover {
-    object-fit: cover;
-    height: 100%;
-    width: 100%;
-    border-radius: 0 !important;
-  }
-
-  .ant-card,
-  .ant-card-body {
-    border-radius: 0 !important;
-  }
-
-  .ant-card-body {
-    padding: 12px;
-  }
-
-  .ant-card-meta {
-    margin: 0;
-  }
-
-  .instructions {
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    margin-bottom: 12px;
-  }
-
   /* styles for tablets */
   @media (min-width: 768px) {
     .card-group {
       grid-template-columns: repeat(2, minmax(0, 1fr));
       grid-gap: 12px;
-    }
-
-    .card {
-      margin-bottom: 0 !important;
     }
   }
 
