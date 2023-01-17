5cc8fecdf7ec75990c490f8cce7635cc192df58c
Akash Perera <akash.perera@chope.co>
2023-01-18 00:51:06 +0530

    add default layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import "antd/dist/reset.css";
 import { Route, Routes } from "react-router-dom";
 import FavoritesPage from "./routes/favorites";
 import IndexPage from "./routes/index";
+import Layout from "./routes/layout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Route>
