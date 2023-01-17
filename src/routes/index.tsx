31b273bcf493bcaae043223e280bf222455b1f05
Akash Perera <akash.perera@chope.co>
2023-01-18 00:51:12 +0530

    fix styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -59,7 +59,7 @@ const Container = styled.div`
     grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 
-  .card {
+  .card:not(:last-child) {
     margin-bottom: 12px;
   }
 
@@ -106,11 +106,10 @@ const Container = styled.div`
     .card-group {
       grid-template-columns: repeat(2, minmax(0, 1fr));
       grid-gap: 12px;
-      margin-bottom: 12px;
     }
 
     .card {
-      margin-bottom: 0;
+      margin-bottom: 0 !important;
     }
   }
 
